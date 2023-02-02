fc93427a20895e5c36fdb180fa474f92e245adb4
BEZYL MOPHAT OTIENO <96384227+Bezyl-Mophat-Otieno@users.noreply.github.com>
2023-02-02 06:53:01 +0000

    Just made sure the login modal was working correctly

--- src/Components/LandingPageComponents/LoginModal.jsx
+++ src/Components/LandingPageComponents/LoginModal.jsx
@@ -6,46 +6,48 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faIdBadge } from "@fortawesome/free-solid-svg-icons"
 import RegisterForm from "./RegisterForm"
 import { Modal } from "react-bootstrap"
+import { useState } from "react"
+import Button from "react-bootstrap"
 
- function LoginModal () {
- const {loginSuccess,setLoginSuccess, renderLogModal,setrenderLogModal } = useContext(GlobalContext)
- const toggleModal = ()=>{
-  setrenderLogModal((prev)=>{
-    return !prev
-  })
+function LoginModal() {
+  const { loginSuccess, setLoginSuccess, renderLogModal, setrenderLogModal } = useContext(GlobalContext)
+  const toggleModal = () => {
+    setrenderLogModal((prev) => {
+      return !prev
+    })
 
- }
-   const [show, setShow] = useState(false);
+  }
+  const [show, setShow] = useState(false);
 
   const handleClose = () => setShow(false);
 
-      return ( 
+  return (
     <>
-<div className = 'login-modal'>
-                    <Modal show={show} onHide={handleClose}>
-                      <Modal.Header closeButton>
-                      <h5 class="modal-title" id="exampleModalLabel"> <FontAwesomeIcon icon={faIdBadge} /> </h5>
-                      </Modal.Header>
-                      <Modal.Body>    
-              {loginSuccess ===!undefined && <LoginSuccess/>}
-              {renderLogModal ? <LoginForm/> : <RegisterForm/> }
-              <div className="d-flex justify-content-center"> Do you have an account ?
-               <a href="#" className="text-primary " onClick={toggleModal} >{ renderLogModal ? " Register." : " Login."}</a></div></Modal.Body>
-                     <Modal.Footer className='modal-footer'>
-                        <Button variant="secondary" onClick={handleClose}>
-                          Close
-                      </Button>
-              {/* <button type="button" class="btn btn-secondary btn-danger" data-bs-dismiss="modal" onClick={()=>{setLoginSuccess()
+      <div className='login-modal'>
+        <Modal show={show} onHide={handleClose}>
+          <Modal.Header closeButton>
+            <h5 class="modal-title" id="exampleModalLabel"> <FontAwesomeIcon icon={faIdBadge} /> </h5>
+          </Modal.Header>
+          <Modal.Body>
+            {loginSuccess === !undefined && <LoginSuccess />}
+            {renderLogModal ? <LoginForm /> : <RegisterForm />}
+            <div className="d-flex justify-content-center"> Do you have an account ?
+              <a href="#" className="text-primary " onClick={toggleModal} >{renderLogModal ? " Register." : " Login."}</a></div></Modal.Body>
+          <Modal.Footer className='modal-footer'>
+            <Button variant="secondary" onClick={handleClose}>
+              Close
+            </Button>
+            {/* <button type="button" class="btn btn-secondary btn-danger" data-bs-dismiss="modal" onClick={()=>{setLoginSuccess()
               setrenderLogModal(true)}}>Close</button> */}
-                     </Modal.Footer>
-                   </Modal>
-             </div>
+          </Modal.Footer>
+        </Modal>
+      </div>
     </>
-      )
+  )
 
-      
 
-    
+
+
 }
 
 export default LoginModal
